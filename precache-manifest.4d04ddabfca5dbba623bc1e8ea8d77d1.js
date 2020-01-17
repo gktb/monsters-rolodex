b922caa76287a9a42e567db094ff0fabb0771d3b
gktb <gokcen.tonbul@gmail.com>
2020-01-17 21:03:13 +0300

    Updates

---
+++ precache-manifest.4d04ddabfca5dbba623bc1e8ea8d77d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c87b7298f7eb6a1ccea84d7d1bc51c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62be6eafa710d7fc3b49",
+    "url": "/monsters-rolodex/static/css/main.668cad21.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62be6eafa710d7fc3b49",
+    "url": "/monsters-rolodex/static/js/main.158ac710.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
